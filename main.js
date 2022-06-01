7d0c1c638bc8b0a7883de52eb16495dd88ba6344
Fatma3112 <105184081+Fatma3112@users.noreply.github.com>
2022-06-01 22:10:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,15 +2,33 @@ let count = 0;
 let save = document.getElementById("par");
 let countNumber = document.getElementById("count");
 
+let btns = document.querySelectorAll(".btn");
 
-  function counEL() {
-     
-    countNumber.innerText = count++;
+
+btns.forEach(function(btn) {
+btn.addEventListener("click", function(e) {
+  const style = e.currentTarget.classList;
+  if(style.contains("btne")) {
+    count++;
+  } else if(style.contains("decrease")) {
+    count--;
+  } else if(style.contains("reset")) {
+    count = 0;
+  }
+ countNumber.textContent = count;
+ 
+  if(style.contains("butt")) {
+  let countS = count + " / ";  
+   save.textContent += countS ;
+   countNumber.textContent = 0; 
+ }
+if(count > 0) {
+  countNumber.style.color = "green";
 }
- function saveEl(){
-    let countS = count + " / " ;
-    save.textContent  += countS;
-    countNumber.textContent = 0;
-    count= 0;
-    
+if(count < 0 ) {
+  countNumber.style.color = "purple";
 }
+
+})
+})
+  
